e3536989d0370e827bfb89d7fd2f10b64da3f672
vicky <tyagivivek528@gmail.com>
2023-07-31 17:27:38 +0530

    Geek for geek is arrays are equal?

---
+++ array_equal.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end());
+
+    for (int i = 0; i < N; i++)
+    {
+        if (A[i] != B[i])
+        {
+            return false;
+        }
+    }
+    return true;
+
+    return 0;
+}
